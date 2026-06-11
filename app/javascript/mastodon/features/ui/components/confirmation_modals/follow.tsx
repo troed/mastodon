29e6a763c736c754c30631db96b3349dc19527a0
Roni Laukkarinen <roni@dude.fi>
2026-06-11 22:23:20 +0300

    Restyle follow confirmation as compact card with name, avatar, bio and follower count

--- app/javascript/mastodon/features/ui/components/confirmation_modals/follow.tsx
+++ app/javascript/mastodon/features/ui/components/confirmation_modals/follow.tsx
@@ -1,26 +1,25 @@
 import { useCallback, useEffect } from 'react';
 
-import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+import { defineMessages, useIntl } from 'react-intl';
 
 import { Link } from 'react-router-dom';
 
 import { fetchAccount, followAccount } from 'mastodon/actions/accounts';
 import { AccountBio } from 'mastodon/components/account_bio';
 import { Avatar } from 'mastodon/components/avatar';
+import { Button } from 'mastodon/components/button';
 import { FollowersCounter } from 'mastodon/components/counters';
 import { DisplayName } from 'mastodon/components/display_name';
+import { LoadingIndicator } from 'mastodon/components/loading_indicator';
+import { ModalShell, ModalShellBody } from 'mastodon/components/modal_shell';
 import { ShortNumber } from 'mastodon/components/short_number';
 import { domain } from 'mastodon/initial_state';
 import { useAppDispatch, useAppSelector } from 'mastodon/store';
 
 import type { BaseConfirmationModalProps } from './confirmation_modal';
-import { ConfirmationModal } from './confirmation_modal';
 
 const messages = defineMessages({
-  followConfirm: {
-    id: 'account.follow',
-    defaultMessage: 'Follow',
-  },
+  follow: { id: 'account.follow', defaultMessage: 'Follow' },
 });
 
 export const ConfirmFollowModal: React.FC<
@@ -38,47 +37,60 @@ export const ConfirmFollowModal: React.FC<
     }
   }, [dispatch, accountId, account]);
 
-  const onConfirm = useCallback(() => {
-    dispatch(followAccount(accountId));
-  }, [dispatch, accountId]);
+  const handleSubmit = useCallback(
+    (event: React.FormEvent) => {
+      event.preventDefault();
+      onClose();
+      dispatch(followAccount(accountId));
+    },
+    [dispatch, accountId, onClose],
+  );
 
   return (
-    <ConfirmationModal
-      title={
-        <FormattedMessage
-          id='confirmations.follow.title'
-          defaultMessage='Follow {name}?'
-          values={{ name: `@${account?.acct}` }}
-        />
-      }
-      confirm={intl.formatMessage(messages.followConfirm)}
-      onConfirm={onConfirm}
-      onClose={onClose}
+    <ModalShell
+      className='follow-confirmation-modal'
+      onSubmit={handleSubmit}
+      aria-label={intl.formatMessage(messages.follow)}
     >
-      {account && (
-        <div className='follow-confirmation-card'>
-          <Link
-            to={`/@${account.acct}`}
-            className='follow-confirmation-card__name'
-            onClick={onClose}
-          >
-            <Avatar account={account} size={64} />
-            <DisplayName account={account} localDomain={domain} />
-          </Link>
+      <ModalShellBody className='follow-confirmation-card'>
+        {account ? (
+          <>
+            <div className='follow-confirmation-card__header'>
+              <Link
+                to={`/@${account.acct}`}
+                className='follow-confirmation-card__names'
+                onClick={onClose}
+              >
+                <DisplayName account={account} localDomain={domain} />
+              </Link>
 
-          <AccountBio
-            accountId={account.id}
-            className='follow-confirmation-card__bio'
-          />
+              <Avatar account={account} size={64} />
+            </div>
 
-          <div className='follow-confirmation-card__numbers'>
-            <ShortNumber
-              value={account.followers_count}
-              renderer={FollowersCounter}
+            <AccountBio
+              accountId={account.id}
+              className='follow-confirmation-card__bio'
             />
-          </div>
-        </div>
-      )}
-    </ConfirmationModal>
+
+            <div className='follow-confirmation-card__numbers'>
+              <ShortNumber
+                value={account.followers_count}
+                renderer={FollowersCounter}
+              />
+            </div>
+
+            <Button
+              type='submit'
+              className='follow-confirmation-card__button'
+              onClick={handleSubmit}
+            >
+              {intl.formatMessage(messages.follow)}
+            </Button>
+          </>
+        ) : (
+          <LoadingIndicator />
+        )}
+      </ModalShellBody>
+    </ModalShell>
   );
 };
